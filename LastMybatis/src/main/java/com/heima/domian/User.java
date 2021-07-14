801b9e218f59befcb5d5b3b4d0f8a52b57112742
yubinchen <851636527>
2021-07-14 12:56:39 +0800

    将本地工程添加到本地仓库里面去

--- LastMybatis/src/main/java/com/heima/domian/User.java
+++ LastMybatis/src/main/java/com/heima/domian/User.java
@@ -1,7 +1,10 @@
 package com.heima.domian;
 
 import java.io.Serializable;
+import java.util.Date;
 
 public class User implements Serializable{
    private Integer id;
+   private String username;
+   private Date birthday;
 }
